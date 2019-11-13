1ff982fc0dfe72ab2e9ab73860621bdab8e98b1c
Romario Gustave <romagustave10@gmail.com>
2019-11-13 09:38:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,19 @@
 # Write your code here.
-def line() 
+def line
   if number= 0 
+    puts "The line is currently empty."
+  else
   while 
-  number = 1
   counter = 0
   puts "you are number #{number}"
   counter += 1
 end 
+
+def take_a_number(array, string)
+  name = name.collect
+  puts "#{name} you are number #{number}" 
+end
+def now_serving
+  next_person = next_person.chomp
+  puts "the line is currently: 1.#{na}#{next_person}"
+  
